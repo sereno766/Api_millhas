9a102df15df2c102786738b15da86316b5346302
acalu sereno <acalusereno@hotmail.com>
2025-10-14 18:23:50 -0300

    novas rotas
    rota para deletar cliente
    rota para adicionar mais milhas ao cliente

--- routes/usuario_route.py
+++ routes/usuario_route.py
@@ -23,7 +23,26 @@ def post_cliente():
     return jsonify({"id_cliente": novo_id, "mensagem": "Cliente adicionado com sucesso!"}), 201
 
 # retornar um cliente pelo ID
-@usuario_route.route("/clientes/<int:id_usuario>", methods=["GET"])
-def get_cliente(id_usuario):
-    cliente = buscar_cliente(id_usuario)
+@usuario_route.route("/clientes/<int:id_cliente>", methods=["GET"])
+def get_cliente(id_cliente):
+    cliente = buscar_cliente(id_cliente)
     return jsonify(cliente)
+# atualizar um cliente pelo ID
+@usuario_route.route("/clientes/<int:id_cliente>", methods=["PUT"])
+def put_cliente(id_cliente):
+    dados = request.get_json()
+    dados['id_cliente'] = id_cliente
+    update_cliente(dados)
+    return jsonify({"mensagem": "Cliente atualizado com sucesso!"}), 200
+# deletar um cliente pelo ID
+@usuario_route.route("/clientes/<int:id_cliente>", methods=["DELETE"])
+def delet_cliente(id_cliente):
+    deletar_cliente(id_cliente)
+    return jsonify({"mensagem": "Cliente deletado com sucesso!"}), 200
+# adicionar milhas a um cliente pelo ID
+@usuario_route.route("/clientes/<int:id_cliente>/adicionar-milhas", methods=["POST"])
+def adicionar_milhas_cliente(id_cliente):
+    dados = request.get_json()
+    milhas = dados.get("milhas", 0) 
+    adicionar_milhas(id_cliente, milhas)
+    return jsonify({"mensagem": f"{milhas} milhas adicionadas ao cliente {id_cliente}"}), 200
--- services/usuario_service.py
+++ services/usuario_service.py
@@ -38,3 +38,45 @@ def adicionar_cliente(dados):
     cursor.close()
     con.close()
     return novo_id
+
+def update_cliente(dados):
+    con = conectar()
+    cursor = con.cursor()
+    sql = """
+        UPDATE cliente
+        SET NOME = %s, EMAIL = %s, CARTAO = %s, SALDO_MILHAS = %s, DESTINO_DESEJADO = %s
+        WHERE ID_cliente = %s
+    """
+    valores = (
+        dados['nome'],
+        dados['email'],
+        dados['cartao'],
+        dados['saldo_milhas'],
+        dados['destino_desejado'],
+        dados['id_cliente']
+    )
+    cursor.execute(sql, valores)
+    con.commit()
+    cursor.close()
+    con.close()
+    return True
+
+def deletar_cliente(id_cliente):
+    con = conectar()
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM cliente WHERE id_cliente = %s", (id_cliente,))
+    con.commit()
+    cursor.close()
+    con.close()
+    return True
+
+def adicionar_milhas(id_cliente, milhas):
+    con = conectar()
+    cursor = con.cursor()
+    sql = "UPDATE cliente SET SALDO_MILHAS = SALDO_MILHAS + %s WHERE ID_cliente = %s"
+    valores = (milhas, id_cliente)
+    cursor.execute(sql, valores)
+    con.commit()
+    cursor.close()
+    con.close()
+    return True
