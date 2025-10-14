368ed56833944b22b6efece595c3a56545aa1a6e
acalu sereno <acalusereno@hotmail.com>
2025-10-14 15:45:24 -0300

    fix nomenclatura do DB
    mudei de usuario para cliente

--- services/usuario_service.py
+++ services/usuario_service.py
@@ -3,16 +3,16 @@ from conn import conectar
 def listar_clientes():
     con = conectar()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM USUARIO")
+    cursor.execute("SELECT * FROM cliente")
     clientes = cursor.fetchall()
     cursor.close()
     con.close()
     return clientes
 
-def buscar_cliente(id_usuario):
+def buscar_cliente(id_cliente):
     con = conectar()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM USUARIO WHERE id_usuario = %s", (id_usuario,))
+    cursor.execute("SELECT * FROM cliente WHERE id_cliente = %s", (id_cliente,))
     cliente = cursor.fetchone()
     cursor.close()
     con.close()
