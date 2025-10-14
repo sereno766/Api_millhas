05eff6ec8403fbbe5f2f0983e5d3fb26338b04b1
acalu sereno <acalusereno@hotmail.com>
2025-10-14 16:25:36 -0300

    nova rota
    rota para cadastrar um novo cliente

--- routes/usuario_route.py
+++ routes/usuario_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from services.usuario_service import *
 
 
@@ -15,9 +15,14 @@ def hello():
 def get_clientes():
     clientes = listar_clientes()
     return jsonify(clientes)
+# adicionar um novo cliente
+@usuario_route.route("/clientes", methods=["POST"])
+def post_cliente():
+    dados = request.get_json()
+    novo_id = adicionar_cliente(dados)
+    return jsonify({"id_cliente": novo_id, "mensagem": "Cliente adicionado com sucesso!"}), 201
 
-
-
+# retornar um cliente pelo ID
 @usuario_route.route("/clientes/<int:id_usuario>", methods=["GET"])
 def get_cliente(id_usuario):
     cliente = buscar_cliente(id_usuario)
--- services/usuario_service.py
+++ services/usuario_service.py
@@ -17,3 +17,24 @@ def buscar_cliente(id_cliente):
     cursor.close()
     con.close()
     return cliente
+
+def adicionar_cliente(dados):
+    con = conectar()
+    cursor = con.cursor()
+    sql = """
+        INSERT INTO cliente (NOME, EMAIL, CARTAO, SALDO_MILHAS, DESTINO_DESEJADO)
+        VALUES (%s, %s, %s, %s, %s)
+    """
+    valores = (
+        dados['nome'],
+        dados['email'],
+        dados['cartao'],
+        dados['saldo_milhas'],
+        dados['destino_desejado']
+    )
+    cursor.execute(sql, valores)
+    con.commit()
+    novo_id = cursor.lastrowid
+    cursor.close()
+    con.close()
+    return novo_id
